7a11b0e29dc976941cca237d8e2fb9b8adc3f1c7
Golubev <golubev.da16@gmail.com>
2021-12-25 20:46:11 +0300

    Initial commit

---
+++ MyHabits/TabBar.swift
@@ -0,0 +1,30 @@
+//
+//  TabBar.swift
+//  MyHabits
+//
+//  Created by Дмитрий Голубев on 25.12.2021.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
